9f2aa929102c863a621f703677ae1b5996c7817d
AxessLab <github@axesslab.com>
2020-12-04 11:36:56 +0100

    css added to buttons

--- src/components/MonthPicker.tsx
+++ src/components/MonthPicker.tsx
@@ -53,9 +53,9 @@ const MonthPicker: React.FC<MonthPickerProps> = (props) => {
 
     return (
         <div>
-            <button className={styles.previousButton} onClick={() => { changeMonthHandler("previous") }}>Previous</button>
+            <button className={`${styles.previousButton} ${styles.monthSelectionButtons}`} onClick={() => { changeMonthHandler("previous") }}>Previous</button>
             <h2 id="month-label" aria-live="assertive">{monthArray[month]}{" "}{year}</h2>
-            <button className={styles.nextButton} onClick={() => { changeMonthHandler("next") }}>Next</button>
+            <button className={`${styles.nextButton} ${styles.monthSelectionButtons}`} onClick={() => { changeMonthHandler("next") }}>Next</button>
         </div>
     );
 };

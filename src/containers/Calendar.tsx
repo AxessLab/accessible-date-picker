237d22347f1c9d42b51e24b571dc4a7137e60a61
Duygu <duygu@axesslab.com>
2020-12-07 10:46:50 +0100

    completed logic for all arrows

--- src/containers/Calendar.tsx
+++ src/containers/Calendar.tsx
@@ -27,6 +27,7 @@ const Calendar: React.FC = () => {
   console.log("Selected date is: ", clickedDate);
 
   const keyDownHandler = (event: React.KeyboardEvent) => {
+    event.preventDefault();
     const eventTarget = event.target as HTMLElement
     const currentElementId = eventTarget.id.slice(-2) as string;
 
@@ -39,23 +40,43 @@ const Calendar: React.FC = () => {
     }
 
     if (event.key === "ArrowLeft") {
-      event.preventDefault();
       if ((document.getElementById(`button-${sliceChecker(currentElementId) - 1}`) as HTMLButtonElement) === null) {
         return;
       } else {
         (document.getElementById(`button-${sliceChecker(currentElementId) - 1}`) as HTMLButtonElement).focus();
       }
     }
-    if (event.key === "ArrowRight") {
-      event.preventDefault();
+    else if (event.key === "ArrowRight") {
       if ((document.getElementById(`button-${sliceChecker(currentElementId) + 1}`) as HTMLButtonElement) === null) {
         return;
       } else {
         (document.getElementById(`button-${sliceChecker(currentElementId) + 1}`) as HTMLButtonElement).focus();
       }
     }
-
-  };
+    else if (event.key === "ArrowDown") {
+      if ((document.getElementById(`button-${sliceChecker(currentElementId) + 7}`) as HTMLButtonElement) === null) {
+        if (eventTarget.parentElement?.parentElement?.nextElementSibling?.firstElementChild?.firstElementChild as HTMLButtonElement) {
+          (eventTarget.parentElement?.parentElement?.nextElementSibling?.firstElementChild?.firstElementChild as HTMLButtonElement).focus();
+        } else {
+          return;
+        }
+      } else {
+        (document.getElementById(`button-${sliceChecker(currentElementId) + 7}`) as HTMLButtonElement).focus();
+      }
+    }
+    else if (event.key === "ArrowUp") {
+      if ((document.getElementById(`button-${sliceChecker(currentElementId) - 7}`) as HTMLButtonElement) === null) {
+        if (eventTarget.parentElement?.parentElement?.previousElementSibling?.firstElementChild?.firstElementChild as HTMLButtonElement) {
+          (eventTarget.parentElement?.parentElement?.previousElementSibling?.firstElementChild?.firstElementChild as HTMLButtonElement).focus();
+          console.log(eventTarget.parentElement?.parentElement?.previousElementSibling?.firstElementChild?.firstElementChild);
+        } else {
+          return;
+        }
+      } else {
+        (document.getElementById(`button-${sliceChecker(currentElementId) - 7}`) as HTMLButtonElement).focus();
+      }
+    }
+  }
 
   return (
     <div className={styles.calendarContainer} role="application">
